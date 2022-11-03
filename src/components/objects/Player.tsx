6b823f65253bbf9f22acad7d3ed2abb10345d57d
Leonhard Laineste <leonhard.laineste@gmail.com>
2022-11-03 23:18:29 +0200

    More comments, separate real player object and visual model object, fix walking sideways on turns

--- src/components/objects/Player.tsx
+++ src/components/objects/Player.tsx
@@ -21,27 +21,38 @@ import * as THREE from 'three'
 
 const Player = () => {
   // Leva options controls
-  const { WALK_SPEED, WALK_BACKWARDS_SPEED, RUN_SPEED, RUN_BACKWARDS_SPEED } =
-    useLevaControls(
-      'Movement speeds',
-      {
-        WALK_SPEED: { label: 'Walk', value: 1.55, min: 0.1, max: 10 },
-        WALK_BACKWARDS_SPEED: {
-          label: 'Walk back',
-          value: 1,
-          min: 0.1,
-          max: 10,
-        },
-        RUN_SPEED: { label: 'Run', value: 4, min: 0.1, max: 10 },
-        RUN_BACKWARDS_SPEED: {
-          label: 'Run back',
-          value: 2.5,
-          min: 0.1,
-          max: 10,
-        },
+  const {
+    WALK_SPEED,
+    WALK_BACKWARDS_SPEED,
+    RUN_SPEED,
+    RUN_BACKWARDS_SPEED,
+    PLAYER_ROTATION_MAX_SPEED,
+  } = useLevaControls(
+    'Movement speeds',
+    {
+      WALK_SPEED: { label: 'Walk', value: 1.55, min: 0.1, max: 10 },
+      WALK_BACKWARDS_SPEED: {
+        label: 'Walk back',
+        value: 1,
+        min: 0.1,
+        max: 10,
       },
-      { collapsed: true }
-    )
+      RUN_SPEED: { label: 'Run', value: 4, min: 0.1, max: 10 },
+      RUN_BACKWARDS_SPEED: {
+        label: 'Run back',
+        value: 2.5,
+        min: 0.1,
+        max: 10,
+      },
+      PLAYER_ROTATION_MAX_SPEED: {
+        label: 'Rotation speed',
+        value: 6,
+        min: 0.5,
+        max: 16,
+      },
+    },
+    { collapsed: true }
+  )
   const [{ ACCELERATION_X, ACCELERATION_Z }, set] = useLevaControls(
     'Movement acceleration',
     () => ({
@@ -61,6 +72,9 @@ const Player = () => {
     },
     { collapsed: true }
   )
+
+  // Object3D
+  const playerRef = useRef<THREE.Object3D>(null)
 
   // Animations
   const [animation, setAnimation] = useState<Animation>({
@@ -118,7 +132,9 @@ const Player = () => {
         ? setAnimation({ name: 'run_right', moveSpeed: RUN_SPEED })
         : setAnimation({ name: 'walk_right', moveSpeed: WALK_SPEED })
     } else {
-      setAnimation({ name: 'idle', moveSpeed: 0 })
+      if (animation.name !== 'idle') {
+        setAnimation({ name: 'idle', moveSpeed: 0 })
+      }
     }
   }, [keysPressed, shiftPressed])
 
@@ -132,13 +148,15 @@ const Player = () => {
   }, [animation])
 
   useFrame((_, delta) => {
-    if (ref.current && orbitRef.current) {
+    if (playerRef.current && orbitRef.current && ref.current) {
       // Update orbitControl target dynamically
       const target = orbitControlTarget
-        .copy(ref.current.position)
-        .setY(ref.current.position.y + 1.5)
+        .copy(playerRef.current.position)
+        .setY(playerRef.current.position.y + 1.5)
       ;(orbitRef.current as OrbitControlsProps).target = target
-      const cameraPosRef = camera.position.sub(ref.current.position)
+
+      // Store camera offset from player position before transforming player position
+      const cameraPosRef = camera.position.sub(playerRef.current.position)
 
       if (keysPressed.forwardLeft) {
         set({ ACCELERATION_X: diagonalValue })
@@ -169,27 +187,39 @@ const Player = () => {
         set({ ACCELERATION_Z: 0 })
       }
 
-      ref.current.translateX(ACCELERATION_X * delta * animation.moveSpeed)
-      ref.current.translateZ(ACCELERATION_Z * delta * animation.moveSpeed)
+      playerRef.current.translateX(ACCELERATION_X * delta * animation.moveSpeed)
+      playerRef.current.translateZ(ACCELERATION_Z * delta * animation.moveSpeed)
 
       // Store player to state
-      useStore.setState({ player: ref.current as THREE.Object3D })
+      useStore.setState({ player: playerRef.current as THREE.Object3D })
 
-      // Rotate player based on camera rotation
-      camera.position.addVectors(ref.current.position, cameraPosRef)
+      // Set new camera position based on stored camera offset after transforming player position
+      camera.position.addVectors(playerRef.current.position, cameraPosRef)
 
+      // Find radian value to rotate on Y axis
       const angleYCameraDirection = Math.atan2(
-        ref.current.position.x - camera.position.x,
-        ref.current.position.z - camera.position.z
+        playerRef.current.position.x - camera.position.x,
+        playerRef.current.position.z - camera.position.z
       )
 
       rotateQuaternion.setFromAxisAngle(rotationAxis, angleYCameraDirection)
 
+      // Rotate player object3D position camera rotation
+      playerRef.current.quaternion.copy(rotateQuaternion)
+
+      // Copy visual model position from player object3D position
+      ref.current.position.copy(playerRef.current.position)
+
+      // Smoothly rotate model quaternion to forward facing direction
       anyKeyPressed &&
-        ref.current.quaternion.rotateTowards(rotateQuaternion, delta * 4)
+        ref.current.quaternion.rotateTowards(
+          rotateQuaternion,
+          delta * PLAYER_ROTATION_MAX_SPEED
+        )
     }
   })
 
+  // Make model cast shadow properly
   useLayoutEffect(() => {
     scene.traverse(
       (obj) => (obj as THREE.Mesh).isMesh && (obj.castShadow = true)
@@ -197,16 +227,20 @@ const Player = () => {
   }, [])
 
   return (
-    <primitive ref={ref} rotation={[0, Math.PI, 0]} object={scene}>
-      <OrbitControls
-        ref={orbitRef}
-        maxPolarAngle={Math.PI / 2.05}
-        minPolarAngle={Math.PI / 16}
-        minDistance={3}
-        maxDistance={6}
-        enablePan={false}
-      />
-    </primitive>
+    <>
+      <object3D ref={playerRef} />
+      <primitive rotation={[0, Math.PI, 0]} ref={ref} object={scene}>
+        <OrbitControls
+          ref={orbitRef}
+          maxPolarAngle={Math.PI / 2.05}
+          minPolarAngle={Math.PI / 16}
+          minDistance={3}
+          maxDistance={6}
+          enablePan={false}
+          enableDamping={false}
+        />
+      </primitive>
+    </>
   )
 }
 
