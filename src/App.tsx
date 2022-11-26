5c27f46548bf65315efde5797f8ecb53ed4557a1
Leonhard Laineste <leonhard.laineste@gmail.com>
2022-11-26 17:48:59 +0200

    Add default loader, remove unneeded color el

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import Lights from './components/misc/Lights'
 import Floor from './components/objects/Floor'
 import Player from './components/objects/Player'
-import { Stats } from '@react-three/drei'
+import { Loader, Stats } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { Leva } from 'leva'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const App = () => (
   <>
     <Canvas camera={{ position: [0, 4, 6] }} shadows>
       {/* Misc */}
-      <color attach="background" args={['powderblue']} />
       <Stats />
       <Lights />
 
-      {/* Objects */}
-      <Player />
-      <Floor />
+      <Suspense fallback={null}>
+        {/* Objects */}
+        <Player />
+        <Floor />
+      </Suspense>
     </Canvas>
+    <Loader />
 
     <Leva oneLineLabels />
   </>
